0a732c8c9ae59b3f1f89f28ea6e872ae3ec06ac0
RoshanMistry2006 <roshanrikleomistry2006@gmail.com>
2026-01-05 19:19:17 +0100

    Debug Firebase credentials path

--- db.js
+++ db.js
@@ -3,9 +3,20 @@ const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
   const keyPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-  if (!keyPath) throw new Error("Missing GOOGLE_APPLICATION_CREDENTIALS");
 
-  const serviceAccount = JSON.parse(fs.readFileSync(keyPath, "utf8"));
+  console.log("🔐 Using GOOGLE_APPLICATION_CREDENTIALS:", keyPath);
+  console.log(
+    "🔐 Key file exists:",
+    fs.existsSync(keyPath || "")
+  );
+
+  if (!keyPath) {
+    throw new Error("Missing GOOGLE_APPLICATION_CREDENTIALS");
+  }
+
+  const serviceAccount = JSON.parse(
+    fs.readFileSync(keyPath, "utf8")
+  );
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
