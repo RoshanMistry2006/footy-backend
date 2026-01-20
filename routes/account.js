5c27cbefd90183d052314c0635d07a384b47b95e
RoshanMistry2006 <roshanrikleomistry2006@gmail.com>
2026-01-20 11:59:31 +0100

    Fix account route admin import

--- routes/account.js
+++ routes/account.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const admin = require("firebase-admin");
-const { verifyAuth } = require("../verifyAuth");
 
 const { admin, db } = require("../db");
+const { verifyAuth } = require("../verifyAuth");
 
-// DELETE /account/delete
+// DELETE /api/account/delete
 router.delete("/delete", verifyAuth, async (req, res) => {
   try {
     const uid = req.user.uid;
 
-    // 1) Delete user profile doc if you have one (common path)
-    // If you don't have users/{uid}, this just fails silently.
+    // Optional: delete user profile doc (safe even if not present)
     await db.collection("users").doc(uid).delete().catch(() => {});
 
-    // 2) Delete Firebase Auth user (this is the key Apple requirement)
+    // Required: delete Firebase Auth user
     await admin.auth().deleteUser(uid);
 
     return res.status(200).json({ ok: true });
